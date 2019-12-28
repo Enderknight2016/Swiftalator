83b3956fe804c80cf63ecf03fb7c5b6469e1688d
Nicholas LaBelle <nrlabelle@gmail.com>
2019-12-28 09:22:56 -0500

    Swift Setup

---
+++ Swiftulator/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Swiftulator
+//
+//  Created by Nicholas LaBelle on 12/28/19.
+//  Copyright © 2019 Nicholas LaBelle. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
